1d5495c4fd819a3ef6805ae0dd8bf930e0452e67
Mathias Bynens <mathias@qiwi.be>
2014-05-15 15:50:50 +0200

    package.json: Add `legacy-encoding` keyword

--- tests/tests.js
+++ tests/tests.js
@@ -35,7 +35,7 @@
 
 	// explicitly call `QUnit.module()` instead of `module()`
 	// in case we are in a CLI environment
-	QUnit.module('windows1255');
+	QUnit.module('windows-1255');
 
 	test('windows1255.encode', function() {
 		equal(
